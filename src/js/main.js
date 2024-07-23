8171fc712dcb80c039702b668058b7e914632004
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-07-23 12:30:49 -0500

    Dark Mode added, and it remembers your last mode in localStorage.

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 import '../css/style.css';
+import {darkModeHandler} from "./utils.js";
+
+darkModeHandler();
+
+const startGameButton = document.getElementById('startGame');
+startGameButton.addEventListener('click', () => {
+    console.log('button clicked');
+})
+
+
 
