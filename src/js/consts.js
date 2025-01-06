01e6c02cb4a7bc524d3c501326f1693b696771ee
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-01-06 11:27:19 -0600

    Added some differet words.

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,8 +1,7 @@
-export const WORDS = ['lxd', 'lord', 'nivine', 'beks'];
+export const WORDS = ['lxd', 'lord', 'nivine'];
 export const HINTS = {
   'lxd': 'What is the best department in NLU?',
   'lord': 'Who is the director of the best department in NLU?',
   'nivine': 'Who is the president of NLU?',
-  'beks': 'The best intern in the best department in NLU'
 };
 export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
